d9ac96fb1481af9f84bb05fffa50cc30234269b1
이백행 <dlqorgod@naver.com>
2023-07-22 07:35:14 +0900

    ARRAY[a.menu_no :: numeric]

--- script2/dml/postgres/공통컴포넌트/메뉴.sql
+++ script2/dml/postgres/공통컴포넌트/메뉴.sql
@@ -31,7 +31,6 @@ WITH RECURSIVE search_graph(depth, is_cycle, path, menu_nm, progrm_file_nm, menu
       0
       , false
       , ARRAY[a.menu_no :: numeric]
---      , ARRAY[ROW(a.menu_no, a.menu_nm)]
       , a.menu_nm, a.progrm_file_nm, a.menu_no, a.upper_menu_no, a.menu_ordr, a.menu_dc, a.relate_image_path, a.relate_image_nm
     FROM COMTNMENUINFO a
     WHERE a.menu_no = 0
@@ -39,9 +38,28 @@ WITH RECURSIVE search_graph(depth, is_cycle, path, menu_nm, progrm_file_nm, menu
     SELECT
       sg.depth + 1
       , a.menu_no = ANY(path)
---      , ROW(a.menu_no, g.f2) = ANY(path)
       , path || a.menu_no
---      , path || ROW(a.menu_no, g.menu_nm)
+      , a.menu_nm, a.progrm_file_nm, a.menu_no, a.upper_menu_no, a.menu_ordr, a.menu_dc, a.relate_image_path, a.relate_image_nm
+    FROM COMTNMENUINFO a, search_graph sg
+    WHERE a.upper_menu_no = sg.menu_no AND NOT is_cycle
+    AND a.menu_no > 0
+)
+SELECT * FROM search_graph
+;
+
+WITH RECURSIVE search_graph(depth, is_cycle, path, menu_nm, progrm_file_nm, menu_no, upper_menu_no, menu_ordr, menu_dc, relate_image_path, relate_image_nm) AS (
+    SELECT
+      0
+      , false
+      , ARRAY[ROW(a.menu_no, a.menu_nm)]
+      , a.menu_nm, a.progrm_file_nm, a.menu_no, a.upper_menu_no, a.menu_ordr, a.menu_dc, a.relate_image_path, a.relate_image_nm
+    FROM COMTNMENUINFO a
+    WHERE a.menu_no = 0
+  UNION ALL
+    SELECT
+      sg.depth + 1
+      , ROW(a.menu_no, g.f2) = ANY(path)
+      , path || ROW(a.menu_no, g.menu_nm)
       , a.menu_nm, a.progrm_file_nm, a.menu_no, a.upper_menu_no, a.menu_ordr, a.menu_dc, a.relate_image_path, a.relate_image_nm
     FROM COMTNMENUINFO a, search_graph sg
     WHERE a.upper_menu_no = sg.menu_no AND NOT is_cycle
