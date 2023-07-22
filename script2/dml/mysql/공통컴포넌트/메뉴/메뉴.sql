42e978f6a427814308edf57229ae224edc3e560f
이백행 <dlqorgod@naver.com>
2023-07-22 09:37:04 +0900

    no message

--- script2/dml/mysql/공통컴포넌트/메뉴/메뉴.sql
+++ script2/dml/mysql/공통컴포넌트/메뉴/메뉴.sql
@@ -23,16 +23,48 @@ WHERE 1 = 1
     AND a.upper_menu_no IN (1, 2) /* 상위메뉴번호*/
 ;
 
-WITH RECURSIVE RecursiveHierarchy AS (
-  SELECT id, name, parent_id
-  FROM your_table
-  WHERE id = 1 -- 최상위 노드 ID
+-- https://mariadb.com/kb/en/recursive-common-table-expressions-overview/
 
+WITH RECURSIVE search_graph(depth, menu_nm, progrm_file_nm, menu_no, upper_menu_no, menu_ordr, menu_dc, relate_image_path, relate_image_nm) AS (
+    SELECT
+      0
+      , a.menu_nm, a.progrm_file_nm, a.menu_no, a.upper_menu_no, a.menu_ordr, a.menu_dc, a.relate_image_path, a.relate_image_nm
+    FROM COMTNMENUINFO a
+    WHERE a.menu_no = 0
   UNION ALL
-
-  SELECT t.id, t.name, t.parent_id
-  FROM your_table t
-  INNER JOIN RecursiveHierarchy r ON t.parent_id = r.id
+    SELECT
+      sg.depth + 1
+      , a.menu_nm, a.progrm_file_nm, a.menu_no, a.upper_menu_no, a.menu_ordr, a.menu_dc, a.relate_image_path, a.relate_image_nm
+    FROM COMTNMENUINFO a, search_graph sg
+    WHERE a.upper_menu_no = sg.menu_no
+    AND a.menu_no > 0
 )
-SELECT id, name
-FROM RecursiveHierarchy;
+SELECT * FROM search_graph
+;
+
+WITH RECURSIVE search_graph(depth, is_cycle, path, menu_nm, progrm_file_nm, menu_no, upper_menu_no, menu_ordr, menu_dc, relate_image_path, relate_image_nm) AS (
+    SELECT
+      0
+      , false
+--       , ARRAY[ROW(a.menu_no, a.menu_nm)]
+      , CONCAT(a.menu_no, ',')
+      , a.menu_nm, a.progrm_file_nm, a.menu_no, a.upper_menu_no, a.menu_ordr, a.menu_dc, a.relate_image_path, a.relate_image_nm
+    FROM COMTNMENUINFO a
+    WHERE a.menu_no = 0
+  UNION ALL
+    SELECT
+      sg.depth + 1
+--       , ROW(a.menu_no, a.menu_nm) = ANY(path)
+--       , path || ROW(a.menu_no, a.menu_nm)
+      , NULL
+--       , NULL
+      , CONCAT(sg.menu_no, ',', a.menu_no)
+      , a.menu_nm, a.progrm_file_nm, a.menu_no, a.upper_menu_no, a.menu_ordr, a.menu_dc, a.relate_image_path, a.relate_image_nm
+    FROM COMTNMENUINFO a, search_graph sg
+    WHERE a.upper_menu_no = sg.menu_no
+--     AND NOT is_cycle
+    AND LOCATE(a.menu_no, sg.menu_no) = 0
+    AND a.menu_no > 0
+)
+SELECT * FROM search_graph
+;
