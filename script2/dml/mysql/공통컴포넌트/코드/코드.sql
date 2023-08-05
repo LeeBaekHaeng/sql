86d8ca20294b71e052541052f34994a8ba901ded
이백행 <dlqorgod@naver.com>
2023-08-05 10:05:26 +0900

    comthemaildsptchmanage.SNDNG_RESULT_CODE 메일발신관리.발송결과코드

--- script2/dml/mysql/공통컴포넌트/코드/코드.sql
+++ script2/dml/mysql/공통컴포넌트/코드/코드.sql
@@ -1,14 +1,20 @@
-SELECT
+﻿SELECT
     a.*
 FROM comtccmmncode a
 WHERE 1 = 1
 --     AND CODE_ID_NM LIKE '%회원%' /* comtncmmntyuser.MBER_STTUS 커뮤니티사용자.회원 상태 */
-    AND a.code_id_nm = '회원상태' /* comtncmmntyuser.MBER_STTUS 커뮤니티사용자.회원 상태 */
+--    AND a.code_id_nm = '회원상태' /* comtncmmntyuser.MBER_STTUS 커뮤니티사용자.회원 상태 */
+--     AND CODE_ID_NM LIKE '%발송결과%' /* comthemaildsptchmanage.SNDNG_RESULT_CODE 메일발신관리.발송결과코드 */
+    AND a.code_id_nm = '발송결과구분' /* comthemaildsptchmanage.SNDNG_RESULT_CODE 메일발신관리.발송결과코드 */
 ;
 
 SELECT
-    a.*
+--     a.code_id
+    a.code
+    , a.code_nm
+--     , a.*
 FROM comtccmmndetailcode a
 WHERE 1 = 1
-    AND a.code_id = 'COM013' /* 회원상태 MBER_STTUS */
+--     AND a.code_id = 'COM013' /* 회원상태 MBER_STTUS */
+    AND a.code_id = 'COM024' /* 발송결과구분 SNDNG_RESULT_CODE */
 ;
