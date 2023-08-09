8f34b749a1d1e169309e777687cf11158536c9a8
이백행 <dlqorgod@naver.com>
2023-08-09 22:43:44 +0900

    코드명

---
+++ script2/ddl/postgres/함수/코드명.sql
@@ -0,0 +1,38 @@
+﻿--코드명
+
+--SELECT VERSION();
+
+CREATE
+OR REPLACE
+FUNCTION fn_get_code_nm (
+    in_code_id VARCHAR(6)
+    , in_code VARCHAR(15)
+)
+
+RETURNS VARCHAR(60) AS $$
+
+DECLARE rt_code_nm VARCHAR(60);
+
+BEGIN
+
+
+    SELECT
+        a.code_nm
+        INTO rt_code_nm
+--        , a.*
+    FROM
+        comtccmmndetailcode a
+    WHERE 1 = 1
+        AND a.code_id = in_code_id
+        AND a.code = in_code
+    ;
+
+    RETURN rt_code_nm;
+
+END;
+$$ LANGUAGE plpgsql
+
+
+SELECT
+    fn_get_code_nm('COM001', 'REGC01') code_nm
+;
