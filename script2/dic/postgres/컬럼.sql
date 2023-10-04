3b27dcb3fb1700b624015d913b05d17aa5d390ec
이백행 <bhlee@gsitm.com>
2023-10-04 14:06:30 +0900

    컬럼 코멘트

--- script2/dic/postgres/컬럼.sql
+++ script2/dic/postgres/컬럼.sql
@@ -1,13 +1,87 @@
+/*
+
+https://www.postgresql.org/docs/current/catalogs.html
+
+Chapter 53. System Catalogs
+
+*/
+
 SELECT
-    a.*
+    (
+        SELECT aa.description
+        FROM pg_description aa
+        WHERE aa.objoid = (
+                SELECT aaa.oid
+                FROM pg_class aaa
+                WHERE aaa.oid = (a.table_schema || '.' || a.table_name)::regclass
+            )
+            AND aa.objsubid = 0
+    ) table_comments
+    , (
+        SELECT aa.description
+        FROM pg_description aa
+        WHERE aa.objoid = (
+                SELECT aaa.oid
+                FROM pg_class aaa
+                WHERE aaa.oid = (a.table_schema || '.' || a.table_name)::regclass
+            )
+            AND aa.objsubid = a.ordinal_position
+    ) column_comments
+    , a.*
 FROM
     information_schema.columns a
 WHERE 1 = 1
-    AND table_catalog = 'com'
---    AND table_schema = 'com'
---    AND table_schema = 'public'
---    AND column_name = 'use_at'
-    AND column_name LIKE '%_at'
+--    AND a.table_catalog = 'com'
+--    AND a.table_schema = 'com'
+--    AND a.table_schema = 'public'
+--    AND a.column_name = 'use_at'
+--    AND a.column_name LIKE '%_at'
 ORDER BY 
-    table_catalog, table_schema, table_name, ordinal_position
+    a.table_catalog, a.table_schema, a.table_name, a.ordinal_position
+;
+
+--Table Comments
+SELECT
+    a.*
+FROM
+    pg_description a
+WHERE 1 = 1
+--    AND a.objoid = 'comtcadministcode'::regclass
+--    AND a.objoid = 'com.comtcadministcode'::regclass
+    AND a.objoid = 'public.comtcadministcode'::regclass
+--    AND a.objoid = 'comtcadministcode'::regclass::oid
+    AND objsubid = 0
+;
+
+--Column Comments
+SELECT description 
+FROM pg_description 
+WHERE objsubid = (SELECT ordinal_position 
+                  FROM information_schema.columns 
+                  WHERE table_schema = 'com' AND table_name = 'comtcadministcode' 
+                    AND column_name = 'administ_zone_se') 
+  AND objoid = 'com.comtcadministcode'::regclass;
+
+--Tables
+SELECT a.oid, a.*
+FROM pg_class a
+WHERE 1 = 1
+--    AND a.relname = 'comtcadministcode'
+--    AND a.oid = 'comtcadministcode'::regclass::oid
+--    AND a.oid = 'com.comtcadministcode'::regclass
+    AND a.oid = 'public.comtcadministcode'::regclass
+;
+
+SELECT 
+    'comtcadministcode'::regclass
+    , 'comtcadministcode'::regclass::oid
+;
+
+SELECT
+    *
+FROM
+    pg_attribute
+WHERE 1 = 1
+    AND attrelid = 'comtcadministcode'::regclass
+    AND attnum > 0
 ;
