4b20dfd9d37819655a3c2ba839d1558ea59ab759
이백행 <dlqorgod@naver.com>
2023-12-23 18:29:41 +0900

    시퀀스

--- script2/dml/postgres/System Catalogs 시스템 카탈로그/System Catalogs 시스템 카탈로그 2023-12-23.sql
+++ script2/dml/postgres/System Catalogs 시스템 카탈로그/System Catalogs 시스템 카탈로그 2023-12-23.sql
@@ -54,7 +54,7 @@ SELECT /* DAO.selectList 조회(멀티건) */
 
 SELECT /* DAO.selectList 조회(멀티건) */
        ROW_NUMBER() OVER() RN
---     , 'ALTER FUNCTION ' || A.PROOWNER || '.' || A.PRONAME || ' OWNER TO us_comobj;' ALTER_TABLE_OWNER
+--     , 'ALTER FUNCTION ' || A.PROOWNER || '.' || A.PRONAME || ' OWNER TO us_comobj;' ALTER_FUNCTION_OWNER
      , A.*
   FROM PG_PROC A
  WHERE 1 = 1
@@ -74,4 +74,56 @@ SELECT /* DAO.selectList 조회(멀티건) */
        A.PROOWNER
  ORDER BY
        A.PROOWNER
+;
+
+/* 시퀀스 */
+
+--pg_class    tables, indexes, sequences, views ("relations")
+
+SELECT /* DAO.selectList 조회(멀티건) */
+       ROW_NUMBER() OVER() RN
+     , 'ALTER SEQUENCE ' || (SELECT AA.USENAME FROM PG_SHADOW AA WHERE AA.USESYSID = A.RELOWNER) || '.' || A.RELNAME || ' OWNER TO us_comobj;' ALTER_SEQUENCE_OWNER
+     , A.RELOWNER
+     , (SELECT AA.USENAME FROM PG_SHADOW AA WHERE AA.USESYSID = A.RELOWNER) RELOWNER_USENAME
+     , A.*
+  FROM PG_CLASS A
+ WHERE 1 = 1
+--relkind char        r = ordinary table, i = index, S = sequence, v = view, c = composite type, s = special, t = TOAST table
+--   AND A.RELKIND = 'r' /* r = ordinary table */
+--   AND A.RELKIND = 'i' /* i = index */
+   AND A.RELKIND = 'S' /* S = sequence */
+--   AND A.RELKIND = 'v' /* v = view */
+--   AND A.RELKIND = 'c' /* c = composite type */
+--   AND A.RELKIND = 's' /* s = special */
+--   AND A.RELKIND = 't' /* t = TOAST table */
+/*  */
+--   AND A.PRONAME LIKE 'fn_%'
+ ORDER BY
+       A.RELOWNER, A.RELNAME
+;
+
+SELECT /* DAO.selectList 조회(멀티건) */
+       ROW_NUMBER() OVER() RN
+     , (SELECT AA.USENAME FROM PG_SHADOW AA WHERE AA.USESYSID = A.RELOWNER) RELOWNER_USENAME
+     , COUNT(A.*)
+  FROM PG_CLASS A
+ WHERE 1 = 1
+   AND (SELECT AA.USENAME FROM PG_SHADOW AA WHERE AA.USESYSID = A.RELOWNER) LIKE 'us_%'
+--relkind char        r = ordinary table, i = index, S = sequence, v = view, c = composite type, s = special, t = TOAST table
+--   AND A.RELKIND = 'r' /* r = ordinary table */
+--   AND A.RELKIND = 'i' /* i = index */
+   AND A.RELKIND = 'S' /* S = sequence */
+--   AND A.RELKIND = 'v' /* v = view */
+--   AND A.RELKIND = 'c' /* c = composite type */
+--   AND A.RELKIND = 's' /* s = special */
+--   AND A.RELKIND = 't' /* t = TOAST table */
+/*  */
+ GROUP BY
+       (SELECT AA.USENAME FROM PG_SHADOW AA WHERE AA.USESYSID = A.RELOWNER)
+ ORDER BY
+       (SELECT AA.USENAME FROM PG_SHADOW AA WHERE AA.USESYSID = A.RELOWNER)
+;
+
+--relowner    int4    pg_shadow.usesysid  Owner of the relation
+SELECT AA.USENAME, AA.* FROM PG_SHADOW AA WHERE AA.USESYSID = 24587
 ;
